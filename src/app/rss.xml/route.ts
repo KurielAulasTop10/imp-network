61f6c03490c9a8e7bc2cac01811d6cef1b3a013d
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-03-10 15:13:14 +0000

    feat: RSS

---
+++ src/app/rss.xml/route.ts
@@ -0,0 +1,41 @@
+import RSS from "rss";
+import { getAllPostsFromNotion } from "@/services/posts";
+
+export async function GET() {
+	const feed = new RSS({
+		title: 'Império Network',
+		description: "Feed oficial da Império Network",
+		generator: 'RSS for Node and Next.js',
+		feed_url: 'https://imperionetwork.netlify.app/rss.xml',
+		site_url: 'https://imperionetwork.netlify.app/',
+		managingEditor: 'aventuraland23@gmail.com (Bruno Ramos)',
+		webMaster: 'aventuraland23@gmail.com (Bruno Ramos)',
+		copyright: `Copyright ${new Date()
+			.getFullYear()
+			.toString()}, Império Network`,
+		language: 'pt-BR',
+		pubDate: new Date().toUTCString(),
+		ttl: 60,
+	});
+
+	const allPosts = await getAllPostsFromNotion();
+
+	if (allPosts) {
+		allPosts.sort((postA, postB) => (postA.date > postB.date ? -1 : 1)).splice(0, 12).map(post => {
+			feed.item({
+				title: post.title,
+				description: post.title,
+				url: `https://imperionetwork.netlify.app/posts/${post.id}`,
+				categories: post.categories || [],
+				author: post.author,
+				date: post.date,
+			});
+		});
+	}
+
+	return new Response(feed.xml({ indent: true }), {
+		headers: {
+			'Content-Type': 'application/xml; charset=utf-8',
+		},
+	});
+}
