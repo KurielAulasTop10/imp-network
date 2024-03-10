c2e15c949b9818ee1d87ac321a3676ac79b92432
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-03-10 03:48:24 +0000

    feat: redis system

--- src/libs/notion.ts
+++ src/libs/notion.ts
@@ -1,66 +1,76 @@
 import { NotionAPI } from 'notion-client';
 import { Block } from 'notion-types';
+import redis from './redis';
 
 const notion = new NotionAPI({
-  authToken: process.env.NOTION_AUTH_TOKEN,
+	authToken: process.env.NOTION_AUTH_TOKEN,
 });
 
-export function getRecordMap(id: string) {
-  return notion.getPage(id);
+export async function getRecordMap(id: string) {
+	const cachedData = await redis.get(`recordMap:${id}`);
+	if (cachedData) {
+		return JSON.parse(cachedData);
+	}
+
+	const data = await notion.getPage(id);
+
+	await redis.set(`recordMap:${id}`, JSON.stringify(data), 'EX', 3600);
+
+	return data;
 }
 
 export function mapImageUrl(url: string, block: Block): string | null {
-  if (!url) {
-    return null;
-  }
+	if (!url) {
+		return null;
+	}
 
-  if (url.startsWith('data:')) {
-    return url;
-  }
+	if (url.startsWith('data:')) {
+		return url;
+	}
 
-  // more recent versions of notion don't proxy unsplash images
-  if (url.startsWith('https://images.unsplash.com')) {
-    return url;
-  }
+	// more recent versions of notion don't proxy unsplash images
+	if (url.startsWith('https://images.unsplash.com')) {
+		return url;
+	}
 
-  try {
-    const u = new URL(url);
+	try {
+		const u = new URL(url);
 
-    if (
-      u.pathname.startsWith('/secure.notion-static.com') &&
-      u.hostname.endsWith('.amazonaws.com')
-    ) {
-      if (
-        u.searchParams.has('X-Amz-Credential') &&
-        u.searchParams.has('X-Amz-Signature') &&
-        u.searchParams.has('X-Amz-Algorithm')
-      ) {
-        // if the URL is already signed, then use it as-is
-        return url;
-      }
-    }
-  } catch {
-    // ignore invalid urls
-  }
+		if (
+			u.pathname.startsWith('/secure.notion-static.com') &&
+			u.hostname.endsWith('.amazonaws.com')
+		) {
+			if (
+				u.searchParams.has('X-Amz-Credential') &&
+				u.searchParams.has('X-Amz-Signature') &&
+				u.searchParams.has('X-Amz-Algorithm')
+			) {
+				// if the URL is already signed, then use it as-is
+				return url;
+			}
+		}
+	} catch {
+		// ignore invalid urls
+	}
 
-  if (url.startsWith('/images')) {
-    url = `https://www.notion.so${url}`;
-  }
+	if (url.startsWith('/images')) {
+		url = `https://www.notion.so${url}`;
+	}
 
-  url = `https://www.notion.so${
-    url.startsWith('/image') ? url : `/image/${encodeURIComponent(url)}`
-  }`;
+	url = `https://www.notion.so${
+		url.startsWith('/image') ? url : `/image/${encodeURIComponent(url)}`
+	}`;
 
-  const notionImageUrlV2 = new URL(url);
-  let table = block.parent_table === 'space' ? 'block' : block.parent_table;
-  if (table === 'collection' || table === 'team') {
-    table = 'block';
-  }
-  notionImageUrlV2.searchParams.set('table', table);
-  notionImageUrlV2.searchParams.set('id', block.id);
-  notionImageUrlV2.searchParams.set('cache', 'v2');
+	const notionImageUrlV2 = new URL(url);
+	let table = block.parent_table === 'space' ? 'block' : block.parent_table;
+	if (table === 'collection' || table === 'team') {
+		table = 'block';
+	}
+	notionImageUrlV2.searchParams.set('table', table);
+	notionImageUrlV2.searchParams.set('id', block.id);
+	notionImageUrlV2.searchParams.set('cache', 'v2');
 
-  url = notionImageUrlV2.toString();
+	url = notionImageUrlV2.toString();
 
-  return url;
+	return url;
 }
---
+++ src/libs/redis.ts
@@ -0,0 +1,5 @@
+import Redis from 'ioredis';
+
+const redis = new Redis(`rediss://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_IP}:37389`);
+
+export default redis;
