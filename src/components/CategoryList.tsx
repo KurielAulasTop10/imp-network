8494708a64469f878e8714e757366edb503ce485
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-04-07 19:27:28 +0100

    feat: category link on posts

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
+
 export default function CategoryList({ categories }: { categories: string[] }) {
 	return (
 		<div className="flex space-x-2 flex-wrap">
 			{categories.map(category => (
-				<span
+				<Link
+					href={'/categoria/' + category}
 					key={category}
 					className="bg-red-600 rounded-sm px-4 py-2 md:px-3 md:py-[6px] text-xs uppercase font-semibold"
 				>
 					{category}
-				</span>
+				</Link>
 			))}
 		</div>
 	);
