9d7e19b5d9c995459eac902c22a6101124224522
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-04-20 00:38:11 +0100

    feat: add upslash CDN

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.notion.so',
-      },
-      {
-        protocol: 'https',
-        hostname: 's3-us-west-2.amazonaws.com',
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'www.notion.so',
+			},
+			{
+				protocol: 'https',
+				hostname: 's3-us-west-2.amazonaws.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
