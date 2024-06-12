c1b5b0aa82947092192ce90024f51359417b663e
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-06-13 00:07:00 +0100

    feat: images in webp

--- next.config.js
+++ next.config.js
@@ -3,18 +3,19 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
-				protocol: 'https',
-				hostname: 'www.notion.so',
+				protocol: "https",
+				hostname: "www.notion.so",
 			},
 			{
-				protocol: 'https',
-				hostname: 's3-us-west-2.amazonaws.com',
+				protocol: "https",
+				hostname: "s3-us-west-2.amazonaws.com",
 			},
 			{
-				protocol: 'https',
-				hostname: 'images.unsplash.com',
+				protocol: "https",
+				hostname: "images.unsplash.com",
 			},
 		],
+		formats: ["image/webp"],
 	},
 };
 
