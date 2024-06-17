06509d985eb8c6f94cf856f81352360d19247b7f
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-06-17 20:51:37 +0100

    fixes: dps conserto o search totalmente

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,45 +1,33 @@
 "use client";
 
-import type { ChangeEvent } from "react";
-
 import { BsSearch } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
-import { useRecoilState } from "recoil";
-
-import useFocus from "@/hooks/use-focus";
-import useHover from "@/hooks/use-hover";
-import { queryState } from "@/states/query";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function SearchBar() {
-	const [query, setQuery] = useRecoilState(queryState);
-	const { ref: hoverRef, isHovering } = useHover<HTMLDivElement>();
-	const { ref: focusRef, isFocusing } = useFocus<HTMLInputElement>();
-
-	const handleInputClear = () => {
-		setQuery("");
-	};
-
-	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setQuery(e.target.value);
-	};
+	const [query, setQuery] = useState("");
+	const router = useRouter();
 
 	return (
-		<div ref={hoverRef} className="relative mx-auto md:mx-0 w-full">
+		<div className="relative mx-auto md:mx-0 w-full">
 			<BsSearch className="absolute left-4 flex h-full items-center text-xl text-gray-400" />
-			{query && (isHovering || isFocusing) && (
+			{query && (
 				<IoMdClose
-					onClick={handleInputClear}
+					onClick={() => setQuery("")}
 					className="absolute right-6 flex h-full items-center"
 				/>
 			)}
 			<input
-				ref={focusRef}
 				type="text"
 				placeholder="Pesquisar"
-				onChange={handleInputChange}
+				onChange={(e) => {
+					setQuery(e.target.value);
+					router.push(`/?q=${encodeURIComponent(e.target.value)}`);
+				}}
 				value={query}
 				className={`w-full rounded-md border-[1px] border-black py-2 px-12 text-lg font-normal hover:border-red-600 focus:border-red-600 focus:outline-none bg-black focus:bg-zinc-950 ${
-					isHovering && "border-red-600"
+					query && "border-red-600"
 				}`}
 			/>
 		</div>
--- src/components/posts/PostsGrid.tsx
+++ src/components/posts/PostsGrid.tsx
@@ -1,22 +1,49 @@
+"use client";
+
 import type { PostDocument } from "../../../prismicio-types";
 import PostCard from "@/components/posts/PostCard";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function PostsGrid({ allPosts }: { allPosts: PostDocument[] }) {
+	const [q, setQ] = useState<string>("");
+	const searchParams = useSearchParams();
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		const query = searchParams?.get("q");
+
+		if (query !== undefined && query !== null) {
+			setQ(query);
+		}
+	}, []);
 	return (
 		<section className="flex scroll-mt-12 flex-col items-center space-y-16">
-			{allPosts.length ? (
+			{allPosts.filter((post) =>
+				post.data?.titulo
+					?.toLowerCase()
+					.includes(Array.isArray(q) ? q.join("") : (q || "").toLowerCase()),
+			).length ? (
 				<ul
 					id="posts-grid"
 					className={`grid w-full grid-cols-1 ${allPosts.length < 3 ? `md:grid-cols-${allPosts.length}` : "md:grid-cols-2 xl:grid-cols-3"} gap-x-4 gap-y-5`}
 				>
-					{allPosts.map(
-						(post) =>
-							post && (
-								<li key={post.uid}>
-									<PostCard post={post} />
-								</li>
-							),
-					)}
+					{allPosts
+						.filter((post) =>
+							post.data?.titulo
+								?.toLowerCase()
+								.includes(
+									Array.isArray(q) ? q.join("") : (q || "").toLowerCase(),
+								),
+						)
+						.map(
+							(post) =>
+								post && (
+									<li key={post.uid}>
+										<PostCard post={post} />
+									</li>
+								),
+						)}
 				</ul>
 			) : (
 				<p className="text-center text-lg">Sem resultados</p>
