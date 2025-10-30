e2a4e35119f2d243c8b8f491aa78771d00a623fb
Kazezinhu <3dgamesforever@gmail.com>
2025-10-30 23:19:20 +0000

    feat: remove sentry and clarity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -67,13 +67,6 @@ export default function Footer() {
 		*/}
 				</div>
 			</footer>
-			<Script id="clarity-script" strategy="afterInteractive">
-				{`(function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "mqctv9wnae");`}
-			</Script>
 			<Script
 				src="https://cookieinfoscript.com/js/cookieinfo.min.js"
 				type="text/javascript"
